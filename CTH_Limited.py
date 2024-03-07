209916e1aa2fef31fb4a4b905a519c036ae152cd
nehcneb <nehc.neb@gmail.com>
2024-03-07 11:14:05 +1100

    Update CTH_Limited.py

--- CTH_Limited.py
+++ CTH_Limited.py
@@ -872,7 +872,7 @@ def engage_GPT_json_tokens(questions_json, df_individual, GPT_activation, API_ke
         #Depending on activation status, apply GPT_json function to each judgment, gives answers as a string containing a dictionary
 
         if int(GPT_activation) > 0:
-            GPT_output_list = GPT_json_tokens(questions_json, judgment_json, API_key) #Gives [answers as json, output tokens, input tokens]
+            GPT_output_list = GPT_json_tokens(questions_json, judgment_json, API_key) #Gives [answers as a json, output tokens, input tokens]
             answers_dict = GPT_output_list[0]
         
         else:
@@ -1301,7 +1301,7 @@ if run_button:
         json_output = convert_df_to_json(df_individual)
         
         ste.download_button(
-            label="Download your results as JSON", 
+            label="Download your results as a JSON", 
             data = json_output,
             file_name= output_name + '.json', 
             mime= "application/json", 
